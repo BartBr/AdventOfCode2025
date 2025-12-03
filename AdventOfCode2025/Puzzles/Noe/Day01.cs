f7461b215a0bca9992ec9f96b51752027ca74be2
noegigi <noegigi@gmail.com>
2025-12-03 23:02:24 +0100

    Add Day 1 + Day 2 first part

---
+++ AdventOfCode2025/Puzzles/Noe/Day01.cs
@@ -0,0 +1,85 @@
+using AdventOfCode2025.Common;
+
+namespace AdventOfCode2025.Puzzles.Noe
+{
+	public class Day01 : HappyPuzzleBase<int>
+	{
+		private const int DIAL_SIZE = 100;
+
+		public override int SolvePart1(Input input)
+		{
+			var current = 50;
+			var count = 0;
+			foreach (var line in input.Lines)
+			{
+				var stepCount = int.Parse(line.AsSpan(1)) % DIAL_SIZE;
+				switch (line[0])
+				{
+					case 'L':
+						current -= stepCount;
+						if (current < 0)
+						{
+							current += DIAL_SIZE;
+						}
+						break;
+					case 'R':
+						current += stepCount;
+						if (current >= DIAL_SIZE)
+						{
+							current -= DIAL_SIZE;
+						}
+						break;
+				}
+
+				if (current == 0)
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+
+		public override int SolvePart2(Input input)
+		{
+			var current = 50;
+			var count = 0;
+			foreach (var line in input.Lines)
+			{
+				var stepCount = int.Parse(line.AsSpan(1));
+				switch (line[0])
+				{
+					case 'L':
+						var previous = current;
+						current -= stepCount % DIAL_SIZE;
+						if (current < 0)
+						{
+							current += DIAL_SIZE;
+							if (previous != 0)
+								count++;
+						}
+						break;
+					case 'R':
+						current += stepCount % DIAL_SIZE;
+						if (current >= DIAL_SIZE)
+						{
+							current -= DIAL_SIZE;
+							if (current != 0)
+								count++;
+						}
+						break;
+				}
+
+				count += stepCount / DIAL_SIZE;
+
+				if (current == 0)
+				{
+					count++;
+				}
+
+			}
+
+			return count;
+		}
+	}
+}
