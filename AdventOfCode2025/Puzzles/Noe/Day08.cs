18453addce31d8911080bf00773d4763968189e5
noegigi <noegigi@gmail.com>
2026-01-17 20:10:00 +0100

    Add base for Day 08

---
+++ AdventOfCode2025/Puzzles/Noe/Day08.cs
@@ -0,0 +1,17 @@
+using AdventOfCode2025.Common;
+
+namespace AdventOfCode2025.Puzzles.Noe
+{
+	public class Day08 : HappyPuzzleBase<long>
+	{
+		public override long SolvePart1(Input input)
+		{
+			return 0;
+		}
+
+		public override long SolvePart2(Input input)
+		{
+			return 0;
+		}
+	}
+}
