43c7271263a14d0b6a2c38c8b0c7bcb79ddefde2
noegigi <noegigi@gmail.com>
2026-04-06 20:46:05 +0200

    Attempt at auto validation (Input format not always working)

--- AdventOfCode2025.Console/Program.cs
+++ AdventOfCode2025.Console/Program.cs
@@ -1,19 +1,27 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Text;
 using AdventOfCode2025.Common;
 using AdventOfCode2025.Console;
 using Microsoft.Extensions.Configuration;
 
-var yourName = new ConfigurationBuilder()
+var config = new ConfigurationBuilder()
 	.SetBasePath(Directory.GetCurrentDirectory())
 	.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
 	.AddUserSecrets<Program>(optional: true, reloadOnChange: false)
-	.Build()["YourName"] ?? throw new ValidationException("Could not find a 'YourName' key in appsettings.json");
+	.Build();
+var yourName = config["YourName"] ?? throw new ValidationException("Could not find a 'YourName' key in appsettings.json");
 
 Console.WriteLine($"Hello {yourName}");
 
 var activatedPuzzleRecords = HappyPuzzleHelpers
-	.DiscoverPuzzles(true, yourName)
+	.DiscoverPuzzles(false, yourName)
 	.Select(yourPuzzle => new ActivatorRecord(yourPuzzle.Name, Activator.CreateInstance(yourPuzzle)!));
+
+var client = new HttpClient
+{
+	BaseAddress = new Uri(config["BaseUrl"] ?? throw new ValidationException("Could not find a 'BaseUrl' key in appsettings.json"))
+};
 
 foreach (var puzzleRecord in activatedPuzzleRecords)
 {
@@ -21,12 +29,29 @@ foreach (var puzzleRecord in activatedPuzzleRecords)
 
 	Console.WriteLine("Reading input");
 	var input = Helpers.GetInput(puzzleRecord.Name, yourName);
+	var day = int.Parse(puzzleRecord.Name.AsSpan(3));
+	using var content = new StringContent(
+		input.Text,
+		Encoding.UTF8,
+		"text/plain");
 
-	Console.WriteLine("Solving part 1...");
-	Console.WriteLine(puzzleRecord.SolvePart1(input));
+	using (var response = await client.PostAsync($"/solve/2025/{day}/1", content))
+	{
+		Console.WriteLine("Solving part 1...");
+		var part1 = puzzleRecord.SolvePart1(input).ToString();
+		Console.WriteLine(part1);
+		var confirmedPart1 = await response.Content.ReadAsStringAsync();
+		Debug.Assert(part1 == confirmedPart1);
+	}
 
-	Console.WriteLine("\nSolving part 2...");
-	Console.WriteLine(puzzleRecord.SolvePart2(input));
+	using (var response = await client.PostAsync($"/solve/2025/{day}/2", content))
+	{
+		Console.WriteLine("\nSolving part 2...");
+		var part2 = puzzleRecord.SolvePart2(input).ToString();
+		Console.WriteLine(part2);
+		var confirmedPart2 = await response.Content.ReadAsStringAsync();
+		Debug.Assert(part2 == confirmedPart2);
 
-	Console.WriteLine();
+		Console.WriteLine();
+	}
 }
